e906fbc4dd6f87a7a6e778033cfbf3ad9bb59e2a
Panthro22 <jordonpaynter22@gmail.com>
2022-01-07 15:41:56 -0700

    Spring boot fitness4Us base made

---
+++ Fitness4Us/src/main/java/com/skilldistillery/fitness/entities/Fitness4UsApplication.java
@@ -0,0 +1,13 @@
+package com.skilldistillery.fitness.entities;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class Fitness4UsApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(Fitness4UsApplication.class, args);
+	}
+
+}
