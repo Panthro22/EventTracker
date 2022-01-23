d3f0b7c862fd3b0e1cd7717a330745de378ec943
Panthro22 <jordonpaynter22@gmail.com>
2022-01-23 16:19:02 -0700

    Updated with cross server talk of all controllers for angular

--- Fitness4Us/src/main/java/com/skilldistillery/fitness/controllers/CardioTrainingController.java
+++ Fitness4Us/src/main/java/com/skilldistillery/fitness/controllers/CardioTrainingController.java
@@ -3,6 +3,7 @@ package com.skilldistillery.fitness.controllers;
 import java.util.List;
 
 import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.web.bind.annotation.CrossOrigin;
 import org.springframework.web.bind.annotation.DeleteMapping;
 import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.PathVariable;
@@ -17,6 +18,7 @@ import com.skilldistillery.fitness.services.CardioTrainingService;
 
 @RestController
 @RequestMapping("api")
+@CrossOrigin({ "*", "http://localhost:4203" })
 public class CardioTrainingController {
 	@Autowired
 	private CardioTrainingService cardioTSvc;
